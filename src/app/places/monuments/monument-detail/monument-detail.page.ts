cf4ed1232d5214d28d7ebbb9335fcf0234a5ffb2
aldorea <adomenechreal@gmail.com>
2019-04-14 17:31:50 +0200

    Adding some button to detail component

--- src/app/places/monuments/monument-detail/monument-detail.page.ts
+++ src/app/places/monuments/monument-detail/monument-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../../shared/places.service';
 import { ActivatedRoute } from '@angular/router';
 import { Place } from 'src/app/models/places.model';
+// import { CallNumber } from '@ionic-native/call-number/ngx';
 
 
 @Component({
@@ -13,20 +14,35 @@ export class MonumentDetailPage implements OnInit {
 
 monument: Place;
 visitado: boolean;
+phone: string;
 
   constructor(private placeService: PlacesService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+            //  private callNumber: CallNumber 
+            ) {
+
     this.visitado = false;
 
   }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe( params => {
-      
-      this.monument = this.placeService.getMonumentById(params['id']);
-      console.log(this.monument)
+      this.monument = this.placeService.getMonumentById(params.id);
+      this.phone = this.monument.getTelefono().toString();
+      console.log(this.monument);
     });
+
   }
+
+  // makeCall() {
+  //   if (this.phone === '0') {
+  //     console.log('El telf es 0')
+  //   } else {
+  //     this.callNumber.callNumber(this.phone, true)
+  //     .then(res => console.log('Launched dialer!', res))
+  //     .catch(err => console.log('Error launching dialer', err));
+  //   }
+  // }
 
 
   // mirar como funciona con params
